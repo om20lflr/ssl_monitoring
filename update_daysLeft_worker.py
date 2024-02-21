5c37f490743b249d0d22d68db60ca95b764e8d63
Lyden Faye Reyes <lydenfaye@BGCs-Mac-mini-6.local>
2024-02-21 19:02:55 +0800

    02-21-2024

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -91,16 +91,21 @@ def update_days_in_db(days_left, name):
 
 def compute_days(Domain):
     value = daysLeft(expirationDate(Domain))
+    logging.info(f"Expiration date for {Domain}: {value}")
     if isinstance(value, str):
-        expiration_date = datetime.strptime(value, '%Y-%m-%d')
-        today = datetime.today()
-        days_left = (expiration_date - today).days
+        try:
+            expiration_date = datetime.strptime(value, '%Y-%m-%d')
+            today = datetime.today()
+            days_left = (expiration_date - today).days
 
-        if days_left <= 7:
-            send_email_alert(Domain, days_left)  # Sending email alert for SSL certificate expiring in 7 days
+            if days_left <= 7:
+                send_email_alert(Domain, days_left)  # Sending email alert for SSL certificate expiring in 7 days
 
-        logging.info("The value is an integer.")
-        return days_left
+            logging.info("The value is an integer.")
+            return days_left
+        except ValueError as e:
+            logging.error(f"Error parsing expiration date for {Domain}: {e}")
+            return 0
     else:
         logging.info("The value is not an integer.")
         return 0
