997cae92a6bdc0d635a302d61f9b763146e73aba
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-08 16:30:00 +0800

    test_autoemail_3.1

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -92,8 +92,9 @@ def sendMail(server="smtp.gmail.com",port=587):
     week_old = 14
 
     for domain in domains:
-        logging.info(f"Domain: {domain} is expiring in {days_left} days")
-        print(f"Domain: {domain} is expiring in {days_left} days")
+        if int(days_left) <= int(week_old):
+            logging.info(f"Domain: {domain} is expiring in {days_left} days")
+            print(f"Domain: {domain} is expiring in {days_left} days")
 
 
 
