87652707c87c1826b0a1635c299614d7f7c03c10
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-08 16:07:02 +0800

    test_autoemail_3.1

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -102,38 +102,38 @@ def sendMail(server="smtp.gmail.com",port=587):
 
 
         html = """\
-            <html>
-            <head></head>
-            <body>
-            <p>Hi Team,<br>
-            Reminder:<br>
-            """
+        <html>
+        <head></head>
+        <body>
+        <p>Hi Team,<br>
+        Reminder:<br>
+        """
 
         d = []
         for domain in domains:
             d.append('Here is the <a href="http://contract.mydomain.com/{0}>link</a> you wanted.'.format(domain.days_left))
             print(d)
             html = html.format('\n'.join(d))
-            """
-            </p>
-            </body>
-            </html>
-            """
+        """
+        </p>
+        </body>
+        </html>
+        """
 
-            part2 = MIMEText(html, 'html')
+        part2 = MIMEText(html, 'html')
 
-            msg.attach(part2)
+        msg.attach(part2)
 
-            # Send the message via local SMTP server.
-            mail = smtplib.SMTP('smtp.gmail.com', 587)
+        # Send the message via local SMTP server.
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
 
-            mail.ehlo()
+        mail.ehlo()
 
-            mail.starttls()
+        mail.starttls()
 
-            mail.login('vhchong@snsoft.my', 'yzlw qeoy flvl zazd')
-            mail.sendmail(me, you, msg.as_string())
-            mail.quit()
+        mail.login('vhchong@snsoft.my', 'yzlw qeoy flvl zazd')
+        mail.sendmail(me, you, msg.as_string())
+        mail.quit()
 
 
 if __name__ == '__main__':
