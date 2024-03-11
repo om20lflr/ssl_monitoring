6c9f9fb87adef50c5970cbbaef48d865b353e312
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-11 11:57:38 +0800

    test_autoemail_3.1

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -138,10 +138,11 @@ def sendMail():
 
 
         if int(days_left) <= int(week_old):
-            d.append('{0} is expiring in {1} days.'.format(domain, days_left))
-            print(d)
-            html = html.format('\r\n'.join(d))
-            print(html)
+            for d in domains:
+                d.append('{0} is expiring in {1} days.'.format(domain, days_left))
+                print(d)
+                html = html.format('\r\n'.join(d))
+                print(html)
 
 
 
