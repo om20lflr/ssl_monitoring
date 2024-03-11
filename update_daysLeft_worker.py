116e46d27ad006708c2fbe53e1961677acecca6c
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-11 16:33:29 +0800

    test_autoemail_3.1

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -119,18 +119,7 @@ def sendMail():
     msg['From'] = me
     msg['To'] = you
 
-    html = """\
-    <html>
-        <head></head>
-            <body>
-                <p>Hi Team,<br><br>
-                Reminder:<br>
-                Please check if Domain listed below need to be renewed.<br><br>
-                
-                </p>
-            </body>
-    </html>
-    """
+
     domains = get_domains_from_db()
     week_old = 14
     d = []
@@ -143,9 +132,22 @@ def sendMail():
             d.append("{0} is expiring in {1} days.".format(domain, days_left))
             print(d)
 
-            html = (html + '\n\n'.join(d))
+            html1 = ('\n\n'.join(d))
 
-            print(html)
+            print(html1)
+
+    html = """\
+        <html>
+            <head></head>
+                <body>
+                    <p>Hi Team,<br><br>
+                    Reminder:<br>
+                    Please check if Domain listed below need to be renewed.<br><br>
+                    {html1}
+                    </p>
+                </body>
+        </html>
+        """
 
 
     part2 = MIMEText(html, 'html')
