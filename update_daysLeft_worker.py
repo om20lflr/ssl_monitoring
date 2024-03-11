888b11030e9cdf847feae2d03214d57f0f8aa1ec
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-11 10:35:50 +0800

    test_autoemail_3.1

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -106,61 +106,63 @@ def compute_days(Domain):
 
 def sendMail():
 
-    domain = get_domains_from_db()
+    domains = get_domains_from_db()
     daysleftss = get_daysleft_in_db()
     s = ''.join(str(x) for x in daysleftss)
     week_old = 14
 
     d = []
 
-    if str(get_daysleft_in_db()) < "14":
+    for domain in domains:
+        days_left = compute_days(domain)
+        if int(days_left) <= int(week_old):
 
-        me = "vhchong@snsoft.my"
-        you = "josephcvh@gmail.com"
+            me = "vhchong@snsoft.my"
+            you = "josephcvh@gmail.com"
 
-        msg = MIMEMultipart('alternative')
-        msg['Subject'] = "Domain Expiry Notice"
-        msg['From'] = me
-        msg['To'] = you
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = "Domain Expiry Notice"
+            msg['From'] = me
+            msg['To'] = you
 
-        html = """\
-        <html>
-            <head></head>
-                <body>
-                    <p>Hi Team,<br>
-                    Reminder:<br>
-                    {0}
-                    </p>
-                </body>
-        </html>
-        """
+            html = """\
+            <html>
+                <head></head>
+                    <body>
+                        <p>Hi Team,<br>
+                        Reminder:<br>
+                        {0}
+                        </p>
+                    </body>
+            </html>
+            """
 
-        for domain in domains:
-            days_left = compute_days(domain)
-            d.append('Domain: {0} is expiring in {1} days.'.format(domain, days_left))
-            print(d)
-            html = '\n'.join(d)
-            print(html)
+            for domain in domains:
 
-
-
-
+                d.append('Domain: {0} is expiring in {1} days.'.format(domain, days_left))
+                print(d)
+                html = '\n'.join(d)
+                print(html)
 
 
-        part2 = MIMEText(html, 'html')
 
-        msg.attach(part2)
 
-        # Send the message via local SMTP server.
-        mail = smtplib.SMTP('smtp.gmail.com', 587)
 
-        mail.ehlo()
 
-        mail.starttls()
+            part2 = MIMEText(html, 'html')
 
-        mail.login('vhchong@snsoft.my', 'yzlw qeoy flvl zazd')
-        mail.sendmail(me, you, msg.as_string())
-        mail.quit()
+            msg.attach(part2)
+
+            # Send the message via local SMTP server.
+            mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+            mail.ehlo()
+
+            mail.starttls()
+
+            mail.login('vhchong@snsoft.my', 'yzlw qeoy flvl zazd')
+            mail.sendmail(me, you, msg.as_string())
+            mail.quit()
 
 
 if __name__ == '__main__':
