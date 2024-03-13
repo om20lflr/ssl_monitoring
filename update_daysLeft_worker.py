15848273240598ab6d089c1c088d162c7f212812
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-13 09:48:42 +0800

    test_autoemail_3.2

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -91,7 +91,7 @@ def sendMail():
     you = "vhchong@snsoft.my"
 
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = "SSL Expiration Notice  " + str(today.strftime("%d/%m/%y"))
+    msg['Subject'] = "SSL Expiration Notice" + str(today.strftime(" %d/%m/%y"))
     msg['From'] = me
     msg['To'] = you
 
