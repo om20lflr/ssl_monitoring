a59b59c6db4197f23dc1b4132f142391da7acfcc
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-13 17:40:34 +0800

    test_autoemail_3.2

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -121,7 +121,7 @@ def sendMail():
 
     for domain in domains:
         d = []
-        week_old = 35
+        week_old = 14
 
         days_left = compute_days(domain)
         if int(days_left) <= int(week_old):
