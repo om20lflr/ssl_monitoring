8eafa1cf8e088959aa5902f48750ed34ee5a1dc7
Chong Voon Hei <chongvoonhei@Chongs-MacBook-Pro.local>
2024-03-13 14:17:15 +0800

    test_autoemail_3.2

--- update_daysLeft_worker.py
+++ update_daysLeft_worker.py
@@ -132,23 +132,23 @@ def sendMail():
             </html>
     """
 
-    for i in ed:
-        edd = list(i)
-        html_body = html_body + str(edd)
-        html = html_body + html_close
+
+    edd = list(ed)
+    html_body = html_body + str(edd)
+    html = html_body + html_close
     #html = ""
 
-        part2 = MIMEText(html, 'html')
-        msg.attach(part2)
+    part2 = MIMEText(html, 'html')
+    msg.attach(part2)
 
 
-        # Send the message via local SMTP server.
-        mail = smtplib.SMTP('smtp.gmail.com', 587)
-        mail.ehlo()
-        mail.starttls()
-        mail.login('noreply-cpom@hotelstotsenberg.com', 'zfuq egca fewo dwul')
-        mail.sendmail(me, you, msg.as_string())
-        mail.quit()
+    # Send the message via local SMTP server.
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login('noreply-cpom@hotelstotsenberg.com', 'zfuq egca fewo dwul')
+    mail.sendmail(me, you, msg.as_string())
+    mail.quit()
 
 
 
